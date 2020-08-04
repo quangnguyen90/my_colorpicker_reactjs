e2f12f27ed9b3ee7f6346ef2ece31c298a0ed33e
quangnguyen90 <nguyenphuquang90@gmail.com>
2020-08-04 23:15:31 +0700

    Init HTML

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-50">
+      <div className="row">
+        <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <div className="panel panel-primary">
+            <div className="panel-heading">
+              <h3 className="panel-title">Color Picker</h3>
+            </div>
+            <div className="panel-body">
+
+              <hr />
+            </div>
+          </div>
+        </div>
+        <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+          <div className="panel panel-warning">
+            <div className="panel-heading">
+              <h3 className="panel-title">Size: 15px</h3>
+            </div>
+            <div className="panel-body">
+              <button type="button" className="btn btn-success">Decrease</button>
+              &nbsp;
+              <button type="button" className="btn btn-success">Increase</button>
+            </div>
+          </div>
+          <button type="button" className="btn btn-primary">Reset</button>
+        </div>
+        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <p>Color: red - Fontsize: 15px</p>
+          <div id="content">
+            Content here
+        </div>
+        </div>
+      </div>
     </div>
   );
 }
