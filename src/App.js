c26e16c67c4b41b4f15dda9db9ab04e5b942c136
quangnguyen90 <nguyenphuquang90@gmail.com>
2020-08-05 02:28:11 +0700

    Finish project

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ class App extends Component {
       fontSize: 15
     }
     this.onSetColor = this.onSetColor.bind(this);
+    this.onChangeSize = this.onChangeSize.bind(this);
+    this.onSettingDefault = this.onSettingDefault.bind(this);
   }
 
   onSetColor(color) {
@@ -21,16 +23,34 @@ class App extends Component {
     });
   }
 
+  onChangeSize(value) {
+    this.setState({
+      fontSize: (this.state.fontSize + value >= 8 && this.state.fontSize <= 36) ? this.state.fontSize + value : this.state.fontSize
+    });
+  }
+
+  onSettingDefault(value) {
+    if (value) {
+      this.setState({
+        color: 'red',
+        fontSize: 15
+      });
+    }
+  }
+
   render() {
     return (
       <div className="container mt-50" >
         <div className="row">
           <ColorPicker color={this.state.color} onReceiveColor={this.onSetColor} />
           <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-            <SizeSetting />
-            <Reset />
+            <SizeSetting
+              fontSize={this.state.fontSize}
+              onChangeSize={this.onChangeSize}
+            />
+            <Reset onSettingDefault={this.onSettingDefault} />
           </div>
-          <Result color={this.state.color} />
+          <Result color={this.state.color} fontSize={this.state.fontSize} />
         </div>
       </div>
     )
--- src/components/SizeSetting.js
+++ src/components/SizeSetting.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 
 class SizeSetting extends Component {
+    changeSize(value) {
+        this.props.onChangeSize(value);
+    }
     render() {
         return (
             <div className="panel panel-warning">
                 <div className="panel-heading">
-                    <h3 className="panel-title">Size: 15px</h3>
+                    <h3 className="panel-title">Size: {this.props.fontSize}</h3>
                 </div>
                 <div className="panel-body">
-                    <button type="button" className="btn btn-success">Decrease</button>
+                    <button type="button" className="btn btn-success" onClick={() => this.changeSize(-2)}>Decrease</button>
                         &nbsp;
-                        <button type="button" className="btn btn-success">Increase</button>
+                    <button type="button" className="btn btn-success" onClick={() => this.changeSize(2)}>Increase</button>
                 </div>
             </div>
         )
