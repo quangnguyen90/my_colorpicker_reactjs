cbaddfbd29591b3381a5f83b74e6436b9285083c
quangnguyen90 <nguyenphuquang90@gmail.com>
2020-08-05 01:19:02 +0700

    Change Color

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import ColorPicker from './components/ColorPicker';
 import Reset from './components/Reset';
 import SizeSetting from './components/SizeSetting';
 import Result from './components/Result';
 
-function App() {
-  return (
-    <div className="container mt-50">
-      <div className="row">
-        <ColorPicker />
-        <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-          <SizeSetting />
-          <Reset />
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: 'red',
+      fontSize: 15
+    }
+    this.onSetColor = this.onSetColor.bind(this);
+  }
+
+  onSetColor(color) {
+    this.setState({
+      color: color
+    });
+  }
+
+  render() {
+    return (
+      <div className="container mt-50" >
+        <div className="row">
+          <ColorPicker color={this.state.color} onReceiveColor={this.onSetColor} />
+          <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <SizeSetting />
+            <Reset />
+          </div>
+          <Result color={this.state.color} />
         </div>
-        <Result />
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App;
